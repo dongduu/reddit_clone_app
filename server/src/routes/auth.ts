1b82cdc13f2829a4c3c05c1414c96db733565f1f
dongduu <4012popo@naver.com>
2022-09-20 18:54:17 +0900

    feat: add auth routes

---
+++ server/src/routes/auth.ts
@@ -0,0 +1,12 @@
+import { Router, Request, Response } from "express";
+
+const register = async (req: Request, res: Response) => {
+  const { email, username, password } = req.body;
+  console.log(email);
+};
+
+const router = Router();
+
+router.post("/register", register);
+
+export default router;
