5c29e721a61c4a37a3c96f9c93eed0da6802e680
dongduu <4012popo@naver.com>
2022-09-20 17:16:47 +0900

    feat: use classnames

--- client/src/components/InputGroup.tsx
+++ client/src/components/InputGroup.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import cls from "classnames";
 
 interface InputGroupProps {
   className?: string;
@@ -22,7 +23,10 @@ const InputGroup: React.FC<InputGroupProps> = ({
       <input
         type={type}
         style={{ minWidth: 300 }}
-        className="w-full p-3 transition duration-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white"
+        className={cls(
+          `w-full p-3 transition duration-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white`,
+          { "border-red-500": error }
+        )}
         placeholder={placeholder}
         value={value}
         onChange={(e) => setValue(e.target.value)}
