fd93a57108fc65e19461a4eaa99665f1a3346e4a
dongduu <4012popo@naver.com>
2022-09-21 18:09:12 +0900

    fix: validation & error

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,6 +2,14 @@ import { validate } from "class-validator";
 import { Router, Request, Response } from "express";
 import User from "../entities/User";
 
+const mapError = (errors: Object[]) => {
+  return errors.reduce((prev: any, err: any) => {
+    prev[err.property] = Object.entries(err.constraint[0][1]);
+
+    return prev;
+  }, {});
+};
+
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
 
@@ -28,6 +36,8 @@ const register = async (req: Request, res: Response) => {
 
     // 엔티티에 정해 놓은 조건으로 user 데이터의 유효성 검사
     errors = await validate(user);
+
+    if (errors.length > 0) return res.status(400).json(mapError(errors));
 
     // 유저 정보를 유저 table에 저장
     await user.save();
