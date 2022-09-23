cf91834e777775f68ebeebb478f6fcfb7b3db658
dongduu <4012popo@naver.com>
2022-09-23 15:35:34 +0900

    feat: add action

--- client/src/context/auth.tsx
+++ client/src/context/auth.tsx
@@ -1,10 +1,15 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 import { User } from "../types";
 
 interface State {
   authenticated: boolean;
   user: User | undefined;
   loading: boolean;
+}
+
+interface Action{
+    type: string;
+    payload: any
 }
 
 // 기본
@@ -17,7 +22,38 @@ const StateContext = createContext<State>({
 // 업데이트용
 const DispatchContext = createContext<any>(null);
 
+const reducer = (state: State, {type, payload}: Action) => {
+    switch(type){
+        case "LOGIN": 
+            return {
+                ...state,
+                authenticated: true,
+                user: payload
+            }
+        case "LOGOUT":
+            return {
+                ...state,
+                authenticated: false,
+                user: null
+            }
+        case "STOPLOADING":
+            return {
+                ...state,
+                loading: false
+            }
+        default:
+            throw new Error(`Unknown action type: ${type}`)
+    }
+}
+
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+
+   const [state, dispatch] = useReducer(reducer, {
+       user: null,
+       authentiated: false,
+       loading: true
+   })
+
   return (
     <DispatchContext.Provider value={dispatch}>
       <StateContext.Provider value={state}>{children}</StateContext.Provider>
