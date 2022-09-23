d57fcdc38a8fd2f2db6e4c6118002be9dde9a8a7
dongduu <4012popo@naver.com>
2022-09-23 14:32:33 +0900

    feat: add context

--- client/src/context/auth.tsx
+++ client/src/context/auth.tsx
@@ -1,7 +1,18 @@
+import { createContext } from "react";
 import { User } from "../types";
 
 interface State {
-  aythenticated: boolean;
+  authenticated: boolean;
   user: User | undefined;
   loading: boolean;
 }
+
+// 기본
+const StateContext = createContext<State>({
+  authenticated: false,
+  user: undefined,
+  loading: true,
+});
+
+// 업데이트용
+const DispatchContext = createContext<any>(null);
