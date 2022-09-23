a0d6f512928b16509f8a8ed501aefc2a3635a4ff
dongduu <4012popo@naver.com>
2022-09-23 15:49:06 +0900

    feat: add dispatch

--- client/src/context/auth.tsx
+++ client/src/context/auth.tsx
@@ -7,9 +7,9 @@ interface State {
   loading: boolean;
 }
 
-interface Action{
-    type: string;
-    payload: any
+interface Action {
+  type: string;
+  payload: any;
 }
 
 // 기본
@@ -22,41 +22,44 @@ const StateContext = createContext<State>({
 // 업데이트용
 const DispatchContext = createContext<any>(null);
 
-const reducer = (state: State, {type, payload}: Action) => {
-    switch(type){
-        case "LOGIN": 
-            return {
-                ...state,
-                authenticated: true,
-                user: payload
-            }
-        case "LOGOUT":
-            return {
-                ...state,
-                authenticated: false,
-                user: null
-            }
-        case "STOPLOADING":
-            return {
-                ...state,
-                loading: false
-            }
-        default:
-            throw new Error(`Unknown action type: ${type}`)
-    }
-}
+const reducer = (state: State, { type, payload }: Action) => {
+  switch (type) {
+    case "LOGIN":
+      return {
+        ...state,
+        authenticated: true,
+        user: payload,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        authenticated: false,
+        user: null,
+      };
+    case "STOPLOADING":
+      return {
+        ...state,
+        loading: false,
+      };
+    default:
+      throw new Error(`Unknown action type: ${type}`);
+  }
+};
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [state, defaultDispatch] = useReducer(reducer, {
+    user: null,
+    authenticated: false,
+    loading: true,
+  });
 
-   const [state, dispatch] = useReducer(reducer, {
-       user: null,
-       authentiated: false,
-       loading: true
-   })
+  const dispatch = (type: string, payload?: any) => {
+    defaultDispatch({ type, payload });
+  };
 
   return (
     <DispatchContext.Provider value={dispatch}>
       <StateContext.Provider value={state}>{children}</StateContext.Provider>
-    </DispatchContext>
+    </DispatchContext.Provider>
   );
 };
